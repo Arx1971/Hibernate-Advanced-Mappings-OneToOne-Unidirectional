2133fdfac9e3358425fff8ea6b05ef57de6a907a
Arx1971 <adnan.rahin13@gmail.com>
2020-02-11 16:11:27 -0500

    update

---
+++ src/main/java/org/hibernate/onetomany/unidirectional/dal/InsertCourseReview.java
@@ -0,0 +1,50 @@
+package org.hibernate.onetomany.unidirectional.dal;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.onetomany.unidirectional.model.Course;
+import org.hibernate.onetomany.unidirectional.model.Instructor;
+import org.hibernate.onetomany.unidirectional.model.InstructorDetail;
+import org.hibernate.onetomany.unidirectional.model.Review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertCourseReview {
+
+    public static void main(String args[]) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class).addAnnotatedClass(Instructor.class).addAnnotatedClass(Review.class)
+                .addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Course course = session.get(Course.class, 10);
+
+            System.out.println(course);
+
+            List<Review> reviews = new ArrayList<>();
+
+            Review review1 = new Review("This is the best Class.");
+            Review review2 = new Review("This class is awesome.");
+            reviews.add(review1);
+            reviews.add(review2);
+
+            course.setReviews(reviews);
+
+            session.save(course);
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+
+}
--- src/main/java/org/hibernate/onetomany/unidirectional/model/Course.java
+++ src/main/java/org/hibernate/onetomany/unidirectional/model/Course.java
@@ -1,6 +1,8 @@
 package org.hibernate.onetomany.unidirectional.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -17,6 +19,20 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        if (reviews == null)
+            reviews = new ArrayList<>();
+        this.reviews = reviews;
+    }
 
     public Course(String title) {
         this.title = title;
@@ -48,6 +64,13 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public void addReview(Review theReview) {
+        if (reviews == null)
+            reviews = new ArrayList<>();
+
+        reviews.add(theReview);
     }
 
     @Override
